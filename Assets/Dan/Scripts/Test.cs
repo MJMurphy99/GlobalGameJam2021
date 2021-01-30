b77a3f2a165a88d5ff36325803a39cec9501b0ef
AraerTalsen <43824879+AraerTalsen@users.noreply.github.com>
2021-01-30 12:54:18 -0500

    The Tunnel is Born

--- Assets/Dan/Scripts/Test.cs
+++ Assets/Dan/Scripts/Test.cs
@@ -6,8 +6,64 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public Tilemap tm;
+    private bool moving;
+    private Vector2 dir = Vector2.zero;
+    private float speed = .25f;
+
+    public TileBase t;
+
+    /*private void Start()
+    {
+        TileBase tb = (TileBase)(Object)t;
+    }*/
+
+    private void Update()
+    {
+        Move();
+    }
 
     private void Move()
+    {
+        if(!moving)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                dir = Vector2.right * Input.GetAxisRaw("Horizontal");
+                StartCoroutine("Digging");
+            }
+            else if (Input.GetAxisRaw("Vertical") != 0)
+            {
+                dir = Vector2.up * Input.GetAxisRaw("Vertical");
+                StartCoroutine("Digging");
+            }
+        }        
+    }
+
+    private IEnumerator Digging()
+    {
+        moving = true;
+
+        float timePassed = 0;
+
+        Vector2 origin = transform.position;
+        Vector2 target = origin + dir;
+
+        while(timePassed < speed)
+        {
+            transform.position = Vector2.Lerp(origin, target, timePassed / speed);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+
+        Vector3Int pos = new Vector3Int((int)target.x, (int)target.y, 0);
+        tm.SetTile(pos, t);
+
+        moving = false;
+    }
+
+    private void DigTunnel()
     {
 
     }
@@ -15,13 +71,5 @@ public class Test : MonoBehaviour
     private void Break()
     {
 
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Vector2 p = collision.GetContact(0).point;
-        Vector3Int v3i = new Vector3Int((int)p.x - 1, (int)p.y, 0);
-        print(v3i);
-        tm.SetTile(v3i, null);
     }
 }

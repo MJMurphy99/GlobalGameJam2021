7e023d371193ffb06a82fed7c30b205e8e389435
Emma <ekhubbell@qu.edu>
2021-01-30 18:03:16 -0500

    Sonar(currently broken)

---
+++ Assets/Emma/sonar/Sonar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour
+{
+    public GameObject ringPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SonarController.Blip += SpawnRing;
+    }
+
+    public void SpawnRing()
+    {
+        GameObject a = Instantiate(ringPrefab);
+        a.transform.position = transform.position;
+        a.GetComponent<Animator>().SetTrigger("Go");
+    }
+}
---
+++ Assets/Emma/sonar/SonarController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarController : MonoBehaviour
+{
+    public static event Action Blip = delegate { };
+    private float coolDown = 30f;
+    private bool cooling = false;
+    float elapsedTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!cooling && Input.GetKeyDown(KeyCode.E))
+        {
+            //Ping();
+            
+            StartCoroutine(Sonar());
+        }
+    }
+
+    IEnumerator Sonar()
+    {
+        elapsedTime = 0f;
+        cooling = true;
+        Blip();
+        yield return new WaitForSeconds(.5f);
+        Blip();
+
+        yield return new WaitForSeconds(10);
+
+        cooling = false;
+    }
+}
